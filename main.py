da3f71bd340e311f952a291467a5f43c6c7d82e1
gccurtis <gccurtis001@gmail.com>
2019-07-22 11:42:58 -0700

    Added some html

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ class mainPage(webapp2.RequestHandler):
     def get(self):
         # self.response.headers['Content-type'] = "Text Plain"
         self.response.write("Hello world!")
+        self.reponse.write("<h2>Hello CSSI!<h2>")
+        self.reponse.write("<em>Hope you are having a fun time<em>")
+        self.reponse.write("<br><br>")
 
 class secretPage(webapp2.RequestHandler):
     def get(self):
