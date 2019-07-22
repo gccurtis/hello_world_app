e62e7e1de98c227892873fd0a390e62ac4dbc02e
gccurtis <gccurtis001@gmail.com>
2019-07-22 12:09:28 -0700

    Added Jinja2

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import webapp2
 import logging
+import jinja2
+import os
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
 
 class mainPage(webapp2.RequestHandler):
-    logging.info("Hi I am a logger");
     def get(self):
+        logging.info("Hi I am a logger")
+        template = jinja_env.get_template("hello.html")
+        self.response.write(template.render())
         # self.response.headers['Content-type'] = "Text Plain"
-        self.response.write("Hello world!")
-        self.reponse.write("<h2>Hello CSSI!<h2>")
-        self.reponse.write("<em>Hope you are having a fun time<em>")
-        self.reponse.write("<br><br>")
+        # self.response.write("Hello world!")
+        # self.reponse.write("<h2>Hello CSSI!<h2>")
+        # self.reponse.write("<em>Hope you are having a fun time<em>")
+        # self.reponse.write("<br><br>")
 
 class secretPage(webapp2.RequestHandler):
     def get(self):
